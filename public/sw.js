d5143f32e19af2d7a2d6cae1ade536fbaf5fd011
SuphatitKKU <suphatit.s@kkumail.com>
2026-09-07 02:28:54 +0700

    Refresh service worker cache for iPad fallback

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "parcel-lab-shell-v3";
+const CACHE_NAME = "parcel-lab-shell-v4";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
