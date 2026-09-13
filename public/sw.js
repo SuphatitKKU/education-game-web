6f6982cb33a6ee9f1653ff65cffa79b06ec5d119
SuphatitKKU <suphatit.s@kkumail.com>
2026-09-14 00:45:39 +0700

    Fix Safari 12 material card rendering

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "parcel-lab-shell-v17-gpt-desktop-orientation";
+const CACHE_NAME = "parcel-lab-shell-v18-safari12-card-colors";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
